9d0899ca48a3624af9790d3bd5652333ccdc9b0e
gonsanto <goncalosantofilipe8@gmail.com>
2026-08-17 15:28:52 +0200

    style: pnpm format closes: #5

--- src/main.ts
+++ src/main.ts
@@ -1,36 +1,40 @@
 import './style.css'
 
-const input = document.querySelector<HTMLInputElement>("#todo-input");
-const button = document.querySelector<HTMLButtonElement>("#add-todo-button");
-const todoList = document.querySelector<HTMLUListElement>("#todo-list");
-const errorMessage = document.querySelector<HTMLParagraphElement>("#todo-error");
+const input = document.querySelector<HTMLInputElement>('#todo-input')
+const button = document.querySelector<HTMLButtonElement>('#add-todo-button')
+const todoList = document.querySelector<HTMLUListElement>('#todo-list')
+const errorMessage = document.querySelector<HTMLParagraphElement>('#todo-error')
 
 if (!input || !button) {
-  throw new Error("One of the input values are null")
+  throw new Error('One of the input values are null')
 }
 
 function check() {
   if (!input || !todoList || !errorMessage) {
-    throw new Error("One of the input values are null")
+    throw new Error('One of the input values are null')
   }
-  errorMessage.textContent = "";
-  input.classList.remove("input--error");
+  errorMessage.textContent = ''
+  input.classList.remove('input--error')
 
-  const inputValue = input.value;
+  const inputValue = input.value
 
-  if (inputValue.trim() === "") {
-    input.classList.add("input--error");
-    errorMessage.textContent = "The input should not be empty !";
-    input.blur();
+  if (inputValue.trim() === '') {
+    input.classList.add('input--error')
+    errorMessage.textContent = 'The input should not be empty !'
+    input.blur()
   } else {
-    const todoElements = document.createElement("li");
-    todoElements.id = "todo-elements"
+    const todoElements = document.createElement('li')
+    todoElements.id = 'todo-elements'
 
-    todoElements.textContent = inputValue;
-    todoList.appendChild(todoElements);
-    input.value = "";
+    todoElements.textContent = inputValue
+    todoList.appendChild(todoElements)
+    input.value = ''
   }
 }
 
-input.addEventListener("keydown", (e: KeyboardEvent): void => { if (e.key === "Enter") { check(); } });
-button.addEventListener("click", check);
+input.addEventListener('keydown', (e: KeyboardEvent) => {
+  if (e.key === 'Enter') {
+    check()
+  }
+})
+button.addEventListener('click', check)
