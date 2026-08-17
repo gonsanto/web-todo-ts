4ccb7fbef502f22b28094b2b61776329b27b74ab
gonsanto <goncalosantofilipe8@gmail.com>
2026-08-17 08:56:20 +0200

    feat: add todo creation closes #5

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,38 @@
-import './style.css'
+import './style.css';
 
-console.log('Hello from typescript')
+const input = document.getElementById("todo-input") as HTMLInputElement;
+const button = document.getElementById("add-todo-button") as HTMLButtonElement;
+const todoList = document.getElementById("todo-list") as HTMLUListElement;
+const errorMessage = document.getElementById("todo-error") as HTMLParagraphElement;
+
+const listTodo: string[] = [];
+
+function check(): void {
+  if (!input || !todoList || !errorMessage) return;
+  errorMessage.textContent = "";
+  input.classList.remove("input--error");
+
+  if (input.value.trim() === "") {
+    errorMessage.textContent = "The input should not be empty !";
+    input.classList.add("input--error");
+    errorMessage.style.color = "rgba(255, 0, 0, 0.7)";
+    input.blur();
+  } else {
+    listTodo.push(input.value.trim());
+
+    todoList.innerHTML = ` 
+      ${listTodo.map((el: string): string => `<li id="todo-elements">${el}</li>`).join("")}
+    `;
+
+    input.value = "";
+    input.focus();
+  }
+}
+
+input.addEventListener("keydown", (e: KeyboardEvent): void => {
+  if (e.key === "Enter") {
+    check();
+  }
+});
+
+button.addEventListener("click", check);
