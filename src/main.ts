58f576444fb0114a2f7433b1e3b3df9d65ff8b1d
gonsanto <goncalosantofilipe8@gmail.com>
2026-08-21 13:03:19 +0200

    refactor: centralize dom in one file and fix todo id duplication

--- src/main.ts
+++ src/main.ts
@@ -1,35 +1,30 @@
 import './style.css'
+import { elements } from './dom.ts'
 
-const input = document.querySelector('#todo-input') as HTMLInputElement
-const button = document.querySelector('#add-todo-button') as HTMLButtonElement
-const todoList = document.querySelector('#todo-list') as HTMLUListElement
-const errorMessage = document.querySelector(
-  '#todo-error',
-) as HTMLParagraphElement
+const { input, button, todoList, errorMessage } = elements
+let todoEl = 0;
 
 function addNewElement() {
   errorMessage.textContent = ''
   input.classList.remove('input--error')
 
   const inputValue = input.value
+  if (!(inputValue.trim() === '')) {
+    const todoElements = document.createElement('li')
 
-  if (inputValue.trim() === '') {
+    todoEl++;
+    todoElements.id = `todo-elements-${todoEl}`
+
+    todoElements.textContent = inputValue
+    todoList.appendChild(todoElements)
+    input.value = ''
+  } else {
     input.classList.add('input--error')
     errorMessage.textContent = 'The input should not be empty !'
     input.blur()
-  } else {
-    const todoElements = document.createElement('li')
-
-    todoElements.id = 'todo-elements'
-    todoElements.textContent = inputValue
-    todoList.appendChild(todoElements)
     input.value = ''
   }
 }
 
-input.addEventListener('keydown', (e: KeyboardEvent) => {
-  if (e.key === 'Enter') {
-    addNewElement()
-  }
-})
+input.addEventListener('keydown', (e: KeyboardEvent) => { if (e.key === 'Enter') { addNewElement() } })
 button.addEventListener('click', addNewElement)
